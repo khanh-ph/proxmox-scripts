670585fcafef56e7f2a9b37cfac7b10dc94175ab
Khanh Pham <khanhpham1091@gmail.com>
2023-10-03 16:37:16 +0700

    Allow to create VM tmpl on file-based storage

--- create-vm-template/script.sh
+++ create-vm-template/script.sh
@@ -4,7 +4,7 @@ ubuntuImageFilename=$(basename $ubuntuImageURL)
 ubuntuImageBaseURL=$(dirname $ubuntuImageURL)
 proxmoxTemplateID="${TMPL_ID:-9000}"
 proxmoxTemplateName="${TMPL_NAME:-ubuntu-2204}"
-scriptTmpPath=/tmp/promox-scripts
+scriptTmpPath=/tmp/proxmox-scripts
 
 init () {
     [ $(id -u) == 0 ] && apt-get update && apt-get install sudo -y || exit 1
@@ -52,8 +52,8 @@ resetMachineID () {
 createProxmoxVMTemplate () {
     sudo qm destroy $proxmoxTemplateID --purge || true
     sudo qm create $proxmoxTemplateID --name $proxmoxTemplateName --memory 2048 --cores 2 --net0 virtio,bridge=vmbr0
-    sudo qm importdisk $proxmoxTemplateID $ubuntuImageFilename $vmDiskStorage
-    sudo qm set $proxmoxTemplateID --scsihw virtio-scsi-single --virtio0 $vmDiskStorage:vm-$proxmoxTemplateID-disk-0
+    sudo qm set $proxmoxTemplateID --scsihw virtio-scsi-single 
+    sudo qm set $proxmoxTemplateID --virtio0 $vmDiskStorage:0,import-from=$scriptTmpPath/$ubuntuImageFilename
     sudo qm set $proxmoxTemplateID --boot c --bootdisk virtio0
     sudo qm set $proxmoxTemplateID --ide2 $vmDiskStorage:cloudinit
     sudo qm set $proxmoxTemplateID --serial0 socket --vga serial0
